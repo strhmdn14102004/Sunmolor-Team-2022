696f879d38a5b50fcd1d5c1ab5d12c38caffc9a6
strhmdn14102004 <strhmdn14102004@gmail.com>
2024-06-14 09:56:19 +0700

    Update chat_page.dart
    update chat page, slide right to edit, slide left to delete message

--- lib/module/chat/chat_page.dart
+++ lib/module/chat/chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:lottie/lottie.dart';
 import 'package:sunmolor_team/helper/dimension.dart';
 import 'package:sunmolor_team/overlay/error_overlay.dart';
@@ -16,6 +17,7 @@ class GroupChatPage extends StatefulWidget {
 
 class _GroupChatPageState extends State<GroupChatPage> {
   final TextEditingController _messageController = TextEditingController();
+  String? _editingMessageId;
 
   void _sendMessage(String text) async {
     try {
@@ -38,15 +40,27 @@ class _GroupChatPageState extends State<GroupChatPage> {
             .get();
         if (userDoc.exists) {
           String fullName = userDoc['fullName'];
-          await FirebaseFirestore.instance
-              .collection('Sunmolor Team Chat Group')
-              .doc('Chat Group')
-              .collection('Pesan')
-              .add({
-            'Pengirim': fullName,
-            'Pesan': text,
-            'Tanggal Dikirim Pesan': FieldValue.serverTimestamp(),
-          });
+          if (_editingMessageId == null) {
+            await FirebaseFirestore.instance
+                .collection('Sunmolor Team Chat Group')
+                .doc('Chat Group')
+                .collection('Pesan')
+                .add({
+              'Pengirim': email, // Ensure this is storing the email
+              'Pesan': text,
+              'Tanggal Dikirim Pesan': FieldValue.serverTimestamp(),
+            });
+          } else {
+            await FirebaseFirestore.instance
+                .collection('Sunmolor Team Chat Group')
+                .doc('Chat Group')
+                .collection('Pesan')
+                .doc(_editingMessageId)
+                .update({'Pesan': text});
+            setState(() {
+              _editingMessageId = null;
+            });
+          }
         }
       }
     } catch (e) {
@@ -59,47 +73,109 @@ class _GroupChatPageState extends State<GroupChatPage> {
     }
   }
 
+  void _deleteMessage(String messageId) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('Sunmolor Team Chat Group')
+          .doc('Chat Group')
+          .collection('Pesan')
+          .doc(messageId)
+          .delete();
+    } catch (e) {
+      Navigator.of(context).push(
+        ErrorOverlay(
+          message: "$e",
+        ),
+      );
+      print('Error deleting message: $e');
+    }
+  }
+
+  void _editMessage(DocumentSnapshot message) {
+    setState(() {
+      _messageController.text = message['Pesan'];
+      _editingMessageId = message.id;
+    });
+  }
+
   Widget _buildMessageItem(DocumentSnapshot message) {
     bool isMe = message['Pengirim'] == FirebaseAuth.instance.currentUser?.email;
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-      child: Column(
-        crossAxisAlignment:
-            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-        children: [
-          Text(
-            message['Pengirim'],
-            style: const TextStyle(fontSize: 12, color: Colors.grey),
-          ),
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment:
-                isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-            children: [
-              if (!isMe)
-                CircleAvatar(
-                  child: Text(message['Pengirim'][0]),
-                ),
-              if (!isMe) const SizedBox(width: 10),
-              Flexible(
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  constraints: BoxConstraints(
-                      maxWidth: MediaQuery.of(context).size.width * 0.7),
-                  decoration: BoxDecoration(
-                    color: isMe ? Colors.blue[200] : Colors.grey[300],
-                    borderRadius: BorderRadius.circular(10),
+    return Dismissible(
+      key: Key(message.id),
+      background: Container(
+        color: Colors.blue,
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Icon(Icons.edit, color: Colors.white),
+      ),
+      secondaryBackground: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Icon(Icons.delete, color: Colors.white),
+      ),
+      confirmDismiss: (direction) async {
+        if (direction == DismissDirection.startToEnd) {
+          // Edit action
+          DateTime sentTime = (message['Tanggal Dikirim Pesan'] as Timestamp).toDate();
+          bool canEdit = DateTime.now().difference(sentTime).inMinutes <= 10;
+          if (canEdit && isMe) {
+            _editMessage(message);
+            return false; // To avoid dismissing the item
+          } else {
+            return false; // Edit not allowed
+          }
+        } else if (direction == DismissDirection.endToStart) {
+          // Delete action
+          if (isMe) {
+            _deleteMessage(message.id);
+            return true; // To dismiss the item
+          } else {
+            return false; // Delete not allowed
+          }
+        }
+        return false;
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        child: Column(
+          crossAxisAlignment:
+              isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          children: [
+            Text(
+              message['Pengirim'],
+              style: const TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+            const SizedBox(height: 5),
+            Row(
+              mainAxisAlignment:
+                  isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+              children: [
+                if (!isMe)
+                  CircleAvatar(
+                    child: Text(message['Pengirim'][0]),
                   ),
-                  child: Text(
-                    message['Pesan'],
-                    style: const TextStyle(color: Colors.black),
+                if (!isMe) const SizedBox(width: 10),
+                Flexible(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.7),
+                    decoration: BoxDecoration(
+                      color: isMe ? Colors.blue[200] : Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      message['Pesan'],
+                      style: const TextStyle(color: Colors.black),
+                    ),
                   ),
                 ),
-              ),
-              if (isMe) const SizedBox(width: 10),
-            ],
-          ),
-        ],
+                if (isMe) const SizedBox(width: 10),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -193,7 +269,7 @@ class _GroupChatPageState extends State<GroupChatPage> {
                       decoration: InputDecoration(
                         fillColor: Colors.black26,
                         hintText: '  Kirim sebuah pesan ...',
-                        hintStyle: const TextStyle(color: Colors.white),
+                        hintStyle: TextStyle(color: Colors.orange[200]),
                         filled: true,
                         contentPadding: const EdgeInsets.all(10),
                         border: OutlineInputBorder(
@@ -205,7 +281,7 @@ class _GroupChatPageState extends State<GroupChatPage> {
                   ),
                 ),
                 IconButton(
-                  icon: const Icon(Icons.send_rounded,),
+                  icon: Icon(Icons.send_rounded, color: Colors.orange[200]),
                   onPressed: () {
                     String text = _messageController.text.trim();
                     if (text.isNotEmpty) {
